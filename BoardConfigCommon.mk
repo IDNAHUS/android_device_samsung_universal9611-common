40ca0fbabb0a84e0c70ba64c226be9af9e5415b5
LuK1337 <priv.luk@gmail.com>
2024-06-21 08:22:25 +0500

    universal9611: vintf: Include device FCM from vendor/lineage
    Sadly we aren't able to include it globally without breaking other devices :(
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,8 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 TARGET_KEYMASTER_VARIANT := samsung
 
 ## Manifest
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 
